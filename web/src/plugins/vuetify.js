e3f864b8b4c5a437dfe5857d3f9cadc4bdae216b
miracleEverywhere <763483966@qq.com>
2025-08-11 10:31:58 +0800

    update

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -11,9 +11,15 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+import {zhHans} from "vuetify/locale";
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'system',
+  },
+  locale: {
+    locale: 'zhHans',
+    messages: {zhHans}
   },
 })
